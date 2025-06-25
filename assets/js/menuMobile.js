a1b479589648dca81d0d96929088b2c23586b3b3
Aislan <aislandnjunior@gmail.com>
2025-06-24 23:59:17 -0300

    Root no css

--- assets/js/menuMobile.js
+++ assets/js/menuMobile.js
@@ -1,35 +1,38 @@
 import outsideClick from './outsideClick.js'
 
-export default function initMenuMobile(){
-    const menuButton = document.querySelector('[data-menu="button"]')
-    const menuList = document.querySelector('[data-menu="list"]')
-    const img = document.querySelector('#menu-imagem-logo')
-    const eventos = ['click', 'touchstart']
+export default function initMenuMobile() {
+  const menuButton = document.querySelector('[data-menu="button"]')
+  const menuList = document.querySelector('[data-menu="list"]')
+  const img = document.querySelector('#menu-imagem-logo')
+  const eventos = ['click', 'touchstart']
 
-    let touchStart = false
-    function openMenu(event){
-        if (event.type === 'touchstart') {
-            event.preventDefault()
-            touchStart = true;
-          }
-        
-          if (event.type === 'click' && touchStart) {
-            touchStart = false;
-            return;
-          }
-        
-          menuList.classList.add('active')
-          menuButton.classList.add('active')
-          img.classList.add('img-disable')
-          
-          outsideClick(menuList, eventos, () => {
-            menuList.classList.remove('active')
-            menuButton.classList.remove('active')
-            img.classList.remove('img-disable')
-          })
-        }
+  if (!menuButton || !menuList) return
 
-    eventos.forEach((userEvent) => {
-        menuButton.addEventListener(userEvent, openMenu)
+  let touchStart = false
+
+  function openMenu(event) {
+    if (event.type === 'touchstart') {
+      event.preventDefault()
+      touchStart = true
+    }
+
+    if (event.type === 'click' && touchStart) {
+      touchStart = false
+      return
+    }
+
+    menuList.classList.add('active')
+    menuButton.classList.add('active')
+    if (img) img.classList.add('img-disable')
+
+    outsideClick(menuList, eventos, () => {
+      menuList.classList.remove('active')
+      menuButton.classList.remove('active')
+      if (img) img.classList.remove('img-disable')
     })
+  }
+
+  eventos.forEach(userEvent =>
+    menuButton.addEventListener(userEvent, openMenu)
+  )
 }
