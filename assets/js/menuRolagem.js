8fb12b5da0203a0ff0a7b85730b2b1c49b28b56c
Aislan <aislandnjunior@gmail.com>
2025-06-26 22:48:41 -0300

    Main com textos e espaçamento corrigido

--- assets/js/menuRolagem.js
+++ assets/js/menuRolagem.js
@@ -1,13 +1,16 @@
 
 export default function initMenuRolagem() {
   const header = document.querySelector(".menu-navbar");
+  const img = document.querySelector("#menu-imagem-logo")
   if (!header) return;
 
   window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
       header.classList.add("transparent");
+      img.src = "assets/images/icones/motor-azul.svg"
     } else {
       header.classList.remove("transparent");
+      img.src = "assets/images/icones/motor-branco.svg"
     }
   });
 }
