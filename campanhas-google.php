fd40d9e057913dd38c1be8fc0b32ff46a68791f8
Aislan <aislandnjunior@gmail.com>
2025-07-29 15:00:25 -0300

    Linkando corretamente

--- campanhas-google.php
+++ campanhas-google.php
@@ -8,7 +8,7 @@
   <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@500;700&family=Oswald:wght@500;700&family=Poppins:wght@700&family=Roboto:wght@400;500;600;700&display=swap" rel="stylesheet">
   <title>Sale Motors | Campanhas de Google</title>
   <link rel="stylesheet" href="assets/css/style.css" />
   <link rel="stylesheet" href="assets/css/paginas/meta-section.css" />
--- sobre.php
+++ sobre.php
@@ -12,6 +12,7 @@
   <title>Sale Motors | Sobre</title>
   <link rel="stylesheet" href="assets/css/style.css" />
   <link rel="stylesheet" href="assets/css/paginas/sobre.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
 <body class="internal">
